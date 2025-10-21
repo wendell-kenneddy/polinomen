5bc859833946be79d94ced8aaef2c92a8e2f9359
Wendell Kenneddy <wkenneddy505@gmail.com>
2025-10-21 19:35:36 -0300

    frontend(dashboard.ecomponent.ts): consume dictionaries service

--- frontend/src/app/modules/dictionaries/components/dashboard/dashboard.component.ts
+++ frontend/src/app/modules/dictionaries/components/dashboard/dashboard.component.ts
@@ -1,12 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { DictionaryCardComponent } from '../dictionary-card/dictionary-card.component';
+import { DictionariesService } from '../../services/dictionaries.service';
+import { Dictionary } from '../../model/dictionary.model';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [DictionaryCardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardComponent {
+  private dictionariesService = inject(DictionariesService);
 
+  protected dictionaries: Dictionary[] = this.dictionariesService.getDictionaries();
 }
